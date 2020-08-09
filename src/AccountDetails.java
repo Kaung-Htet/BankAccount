7eace9061e6c00b15ae0cd7e340b100ebbdec8e4
Kaung-Htet <kaunghtetnyilay@gmail.com>
2020-08-09 16:51:30 +0700

    Add constructors and change methods' coding

--- src/AccountDetails.java
+++ src/AccountDetails.java
@@ -1,11 +1,45 @@
 public class AccountDetails {
-    private int accountNumber;
+    private String accountNumber;
     private double balance;
     private String name;
     private String email;
-    private int phoneNumber;
+    private String phoneNumber;
 
-    public int getAccountNumber() {
+    //ဒါက default constructor လို့ခေါ်တယ်
+    //empty constructor ကိုခေါ်တဲ့အချိန်မှာ သုံးတယ်
+    public AccountDetails(){
+        //this keyword က ဒီ constructor ကနေ တခြား constructor ကိုယူသုံးတာ
+        //this keyword စာကြောင်းက ဒီ code block ထဲမှာ အားလုံးထက် ပထမဦးဆုံးစာကြောင်းမှာရှိရမယ်
+        //မဟုတ်ရင် error ပြလိမ့်မယ်
+        this ("5678", 300, "Default Name", "Default Email", "Default Phone Number");
+        System.out.println("Empty constructor called");
+    }
+
+    //constructor က access modifier နဲ့ class name ပဲနာမည်ပေးရတယ်
+    public AccountDetails (String accountNumber, double balance, String name, String email, String phoneNumber){
+        System.out.println("Account constructor with parameters called");
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void deposit (double depositAmount){
+        this.balance += depositAmount;
+        System.out.println("Deposit of " + depositAmount + " made. New balance is " + this.balance);
+    }
+
+    public void withdrawal (double withdrawalAmount){
+        if ((this.balance - withdrawalAmount) < 0){
+            System.out.println("Only " + this.balance + " available. Withdrawal not processed");
+        } else {
+            this.balance -= withdrawalAmount;
+            System.out.println("Withdrawal of " + withdrawalAmount + " processed. Remaining balance = " + balance);
+        }
+    }
+
+    public String getAccountNumber() {
         return this.accountNumber;
     }
 
@@ -21,11 +55,11 @@ public class AccountDetails {
         return this.email;
     }
 
-    public int getPhoneNumber(){
+    public String getPhoneNumber(){
         return this.phoneNumber;
     }
 
-    public void setAccountNumber(int accountNumber) {
+    public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
     }
 
@@ -41,30 +75,11 @@ public class AccountDetails {
         this.email = email;
     }
 
-    public void setPhoneNumber(int phoneNumber) {
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public double depositFunds (double depositMoney){
-        System.out.println("Your deposited money = " + depositMoney);
-        double balance = getBalance() + depositMoney;
-        setBalance(balance);
-        System.out.print("Your final balance now = " );
-        return getBalance();
-    }
-
-    public double withdrawFunds (double withdrawal){
-        if (withdrawal > getBalance()){
-            System.out.println("Oops your balance ( " + getBalance() + " ) is insufficient");
-        } else {
-            System.out.println("Your withdrawal = " + withdrawal);
-            double balance = getBalance() - withdrawal;
-            setBalance(balance);
-        }
-
-        System.out.print("Your final balance now = " );
-        return getBalance();
-    }
+    
 }
 
 // Create a new class for bank account
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        AccountDetails account = new AccountDetails();
-        System.out.println(account.depositFunds(100));
-        System.out.println(account.withdrawFunds(30));
-        System.out.println(account.depositFunds(250));
+//        AccountDetails bobsAccount = new AccountDetails("12345", 0.00, "Bob Brown",
+//                "myemail@bob.com","(087) 123-4567" );
+
+        AccountDetails bobsAccount = new AccountDetails();
+
+        System.out.println(bobsAccount.getAccountNumber());
+        System.out.println(bobsAccount.getBalance());
+
+//        bobsAccount.setAccountNumber("12345");
+//        bobsAccount.setBalance(0);
+//        bobsAccount.setName("Bob Brown");
+//        bobsAccount.setEmail("myemail@bob.com");
+//        bobsAccount.setPhoneNumber("(087) 123-4567");
+
+        bobsAccount.withdrawal(100);
+
+        bobsAccount.deposit(50);
+        bobsAccount.withdrawal(100);
+
+        bobsAccount.deposit(51);
+        bobsAccount.withdrawal(100);
     }
 }
